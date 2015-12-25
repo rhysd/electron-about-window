b75ce748dd2360154bdd1caf8c65e2463323832c
rhysd <lin90162@yahoo.co.jp>
2015-12-25 11:02:03 +0900

    import from NyaoVim's 'About' window

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+import {BrowserWindow, app, ipcMain} from 'electron';
+import {join} from 'path';
+import assign = require('object-assign');
+
+export default function openAboutWindow(icon_path: string, copyright: string, homepage: string, user_opt: GitHubElectron.BrowserWindowOptions = {}) {
+    const index_html = 'file://' + join(__dirname, 'about.html');
+
+    const options = assign({
+        width: 400,
+        height: 400,
+        useContentSize: true,
+        titleBarStyle: 'hidden-inset',
+    }, user_opt);
+
+    let win = new BrowserWindow(options);
+    win.once('closed', () => {
+        win = null;
+    });
+
+    win.loadURL(index_html);
+    win.webContents.once('dom-ready', () => {
+        win.webContents.send('about-window:info', icon_path, copyright, homepage);
+    });
+
+    return win;
+}
+

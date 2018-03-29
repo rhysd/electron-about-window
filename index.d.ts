88c062ecfc50e75a7727e5bd6c47794a980a2551
rhysd <lin90162@yahoo.co.jp>
2018-03-29 10:59:49 +0900

    update index.d.ts for adding product_name and updating css_path

--- index.d.ts
+++ index.d.ts
@@ -2,6 +2,7 @@
 
 export interface AboutWindowInfo {
     icon_path: string;
+    product_name?: string;
     package_json_dir?: string;
     bug_report_url?: string;
     bug_link_text?: string;
@@ -10,7 +11,7 @@ export interface AboutWindowInfo {
     description?: string;
     license?: string;
     win_options?: Electron.BrowserWindowConstructorOptions;
-    css_path?: string;
+    css_path?: string | string[];
     adjust_window_size?: boolean;
     open_devtools?: boolean;
     use_inner_html?: boolean;

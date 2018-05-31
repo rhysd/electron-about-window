843f85aab96e31bf98be79d5082967fbca60f629
alexanderturinske <a.turinske@gmail.com>
2018-05-30 17:06:58 -0700

    Allow option for hiding version information
    - Fixes #24

--- src/renderer.ts
+++ src/renderer.ts
@@ -61,17 +61,19 @@ ipcRenderer.on('about-window:info', (_: any, info: AboutWindowInfo) => {
             win.setContentSize(width, height + 40);
         }
     }
-});
 
-const versions = document.querySelector('.versions');
-const vs = process.versions;
-for (const name of ['electron', 'chrome', 'node', 'v8']) {
-    const tr = document.createElement('tr');
-    const name_td = document.createElement('td');
-    name_td.innerText = name;
-    tr.appendChild(name_td);
-    const version_td = document.createElement('td');
-    version_td.innerText = ' : ' + vs[name];
-    tr.appendChild(version_td);
-    versions.appendChild(tr);
-}
+    if (info.use_version_info) {
+        const versions = document.querySelector('.versions');
+        const vs = process.versions;
+        for (const name of ['electron', 'chrome', 'node', 'v8']) {
+            const tr = document.createElement('tr');
+            const name_td = document.createElement('td');
+            name_td.innerText = name;
+            tr.appendChild(name_td);
+            const version_td = document.createElement('td');
+            version_td.innerText = ' : ' + vs[name];
+            tr.appendChild(version_td);
+            versions.appendChild(tr);
+        }
+    }
+});
